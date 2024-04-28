05f0d4cff176c77421872a8a9befb7e1629606dd
ibadia <ibad2762@gmail.com>
2024-04-28 19:59:50 +0500

    adding staticfiles

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ docker compose run --rm taxgpt-backend pip install -r requirements.txt
 docker compose up -d --build taxgpt-backend
 docker compose up -d --build celery-worker
 docker compose run --rm taxgpt-backend python manage.py migrate
-
+docker compose run --rm taxgpt-backend python manage.py collectstatic
 # remove danging images
 echo "Running Docker image prune..."
 yes | docker image prune
