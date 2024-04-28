7cb38e707de92c4fbe1fb4197f54c78a78544395
ibadia <ibad2762@gmail.com>
2024-04-28 18:41:27 +0500

    adding docker files and deploy configs

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+# Perform a git pull to update the code
+git pull
+
+# install packages
+docker-compose run --rm taxgpt-backend pip install -r requirements.txt
+
+docker-compose up -d --build taxgpt-backend
+docker-compose up -d --build celery-worker
+docker-compose run --rm taxgpt-backend python manage.py migrate
+
+# remove danging images
+echo "Running Docker image prune..."
+yes | docker image prune
