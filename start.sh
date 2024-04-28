b0e606750f327f0f553d9a5cf276ab3bf7d04b38
ibadia <ibad2762@gmail.com>
2024-04-28 19:53:38 +0500

    turning docker-compose to docker compose for linux

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@
 git pull
 
 # install packages
-docker-compose run --rm taxgpt-backend pip install -r requirements.txt
+docker compose run --rm taxgpt-backend pip install -r requirements.txt
 
-docker-compose up -d --build taxgpt-backend
-docker-compose up -d --build celery-worker
-docker-compose run --rm taxgpt-backend python manage.py migrate
+docker compose up -d --build taxgpt-backend
+docker compose up -d --build celery-worker
+docker compose run --rm taxgpt-backend python manage.py migrate
 
 # remove danging images
 echo "Running Docker image prune..."
